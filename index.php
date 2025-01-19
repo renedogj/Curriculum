4fcb32374e6321ab89ec7c4220f6e2b6515a42ab
renedogj <renedogj@gmail.com>
2025-01-20 00:27:48 +0100

    add certificaciones, css modified

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<title>Javier Renedo</title>
 	<meta name=author content="Javier Renedo">
 
-	<meta http-equiv="refresh" content="1">
+	<!-- <meta http-equiv="refresh" content="1"> -->
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
@@ -16,96 +16,100 @@
 	<link rel="stylesheet" type="text/css" href="css/lenguajes.css">
 </head>
 <body>
-	<div class="div-inicio">
+	<section class="div-inicio">
 		<h1 class="h1-inicioNombre" id="h1-InicioNombre"></h1>
 		<!--<div class="div-inicioFoto"></div>-->
-	</div>
-	<div class="timeline" id="timeline">
-		<div class="experiencia">
-			<div class="container experiencia-2">
-				<div class="content">
-					<h3>Desarrollador de software</h3>
-					<h4>Saints</h4>
-					<p class="periodo">Agosto de 2024 - Actualidad (6 meses)</p>
-					<ul class="ulDescripcion">
-						<li>Desarrollo de aplicaciones IOS y Android usando Ionic con Vue.js</li>
-						<li>Gestion de archivos JSON y bases de datos no relacionales con python</li>
-					</ul>
-					<div class="divTecnologias">
-						<span>JavaScript</span>
-						<span>Ionic</span>
-						<span>Vue.js</span>
-						<span>GitHub</span>
-						<span>JSON</span>
-						<span>Python</span>
-						<span>IOS</span>
-						<span>Android</span>
+	</section>
+	<section class="timeline" id="timeline">
+		<h3>Mi linea del tiempo</h3>
+		<div class="divContenedoraTimeline">
+			<div class="experiencia">
+				<div class="container experiencia-2">
+					<div class="content">
+						<h3>Desarrollador de software</h3>
+						<h4>Saints</h4>
+						<p class="periodo">Agosto de 2024 - Actualidad (6 meses)</p>
+						<ul class="ulDescripcion">
+							<li>Desarrollo de aplicaciones IOS y Android usando Ionic con Vue.js</li>
+							<li>Gestion de archivos JSON y bases de datos no relacionales con python</li>
+						</ul>
+						<div class="divTecnologias">
+							<span>JavaScript</span>
+							<span>Ionic</span>
+							<span>Vue.js</span>
+							<span>GitHub</span>
+							<span>JSON</span>
+							<span>Python</span>
+							<span>IOS</span>
+							<span>Android</span>
+						</div>
+					</div>
+				</div>
+				<div class="container experiencia-1">
+					<div class="content">
+						<h3>Consultor técnico en el Departamento de SAP</h3>
+						<h4>NTT-Data</h4>
+						<p class="periodo">septiembre de 2023 - octubre de 2023 (2 años)</p>
+						<ul class="ulDescripcion">
+							<li>Creación y desarrollo Frontend de aplicaciones conectándolas con aplicaciones Backend (desarrolladas con SAP CAP) utilizando SAP UI5</li>
+							<li>Creación y desarrollo Backend de aplicaciones, haciendo consultas a bases de datos y administrando las mismas utilizando SAP CAP</li>
+
+							<li>Planificación y estimación del desarrollo de las distintas tareas</li>
+
+							<li>Localizar y solucionar con éxito errores encontrados en distintos algoritmos</li>
+
+							<li>Presentar y exponer el avance en las distintas aplicaciones explicando su funcionamiento y como se han realizado, así como creación de material audiovisual como apoyo de la presentación.</li>
+
+							<li>Realización de documentación de proyectos y realización y ejecución de planes de pruebas</li>
+						</ul>
+						<div class="divTecnologias">
+							<span>JavaScript</span>
+							<span>SAP UI5</span>
+							<span>SAP CAP</span>
+							<span>Git</span>
+							<span>JSON</span>
+							<span>SQL</span>
+						</div>
+					</div>
+				</div>
+				<div class="experiencia--1"></div>
+			</div>
+			<div class="formacion">
+				<div class="container formacion-4">
+					<div class="content">
+						<h3>Grado en Ciberseguridad</h3>
+						<h4>Universidad internacional de la Rioja (UNIR)</h4>
+						<p class="periodo">septiembre 2024 – Actualidad</p>
+						<div>
+							<span></span>
+						</div>
+					</div>
+				</div>
+				<div class="container formacion-3">
+					<div class="content">
+						<h3>Master en Ciberseguridad</h3>
+						<h4>ENIIT – UCAM</h4>
+						<p class="periodo">septiembre de 2023 - octubre de 2023 (2 años)</p>
+					</div>
+				</div>
+				<div class="formacion-25"></div>
+				<div class="container  formacion-2">
+					<div class="content">
+						<h3>Técnico Superior de Desarrollo de Aplicaciones WEB</h3>
+						<p class="periodo">junio de 2022</p>
+					</div>
+				</div>
+				<div class="container formacion-1">
+					<div class="content">
+						<h3>Técnico Superior de Desarrollo de Aplicaciones Multiplataforma</h3>
+						<p class="periodo">junio de 2021</p>
 					</div>
 				</div>
 			</div>
-			<div class="container experiencia-1">
-				<div class="content">
-					<h3>Consultor técnico en el Departamento de SAP</h3>
-					<h4>NTT-Data</h4>
-					<p class="periodo">septiembre de 2023 - octubre de 2023 (2 años)</p>
-					<ul class="ulDescripcion">
-						<li>Creación y desarrollo Frontend de aplicaciones conectándolas con aplicaciones Backend (desarrolladas con SAP CAP) utilizando SAP UI5</li>
-						<li>Creación y desarrollo Backend de aplicaciones, haciendo consultas a bases de datos y administrando las mismas utilizando SAP CAP</li>
-
-						<li>Planificación y estimación del desarrollo de las distintas tareas</li>
-
-						<li>Localizar y solucionar con éxito errores encontrados en distintos algoritmos</li>
-
-						<li>Presentar y exponer el avance en las distintas aplicaciones explicando su funcionamiento y como se han realizado, así como creación de material audiovisual como apoyo de la presentación.</li>
-
-						<li>Realización de documentación de proyectos y realización y ejecución de planes de pruebas</li>
-					</ul>
-					<div class="divTecnologias">
-						<span>JavaScript</span>
-						<span>SAP UI5</span>
-						<span>SAP CAP</span>
-						<span>Git</span>
-						<span>JSON</span>
-						<span>SQL</span>
-					</div>
-				</div>
-			</div>
-			<div class="experiencia--1"></div>
 		</div>
-		<div class="formacion">
-			<div class="container formacion-4">
-				<div class="content">
-					<h3>Grado en Ciberseguridad</h3>
-					<h4>Universidad internacional de la Rioja (UNIR)</h4>
-					<p class="periodo">septiembre 2024 – Actualidad</p>
-					<div>
-						<span></span>
-					</div>
-				</div>
-			</div>
-			<div class="container formacion-3">
-				<div class="content">
-					<h3>Master en Ciberseguridad</h3>
-					<h4>ENIIT – UCAM</h4>
-					<p class="periodo">septiembre de 2023 - octubre de 2023 (2 años)</p>
-				</div>
-			</div>
-			<div class="formacion-25"></div>
-			<div class="container  formacion-2">
-				<div class="content">
-					<h3>Técnico Superior de Desarrollo de Aplicaciones WEB</h3>
-					<p class="periodo">junio de 2022</p>
-				</div>
-			</div>
-			<div class="container formacion-1">
-				<div class="content">
-					<h3>Técnico Superior de Desarrollo de Aplicaciones Multiplataforma</h3>
-					<p class="periodo">junio de 2021</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="divCirculosTecnologias">
+	</section>
+	<section class="divCirculosTecnologias">
+		<!-- <h3>Tecnologías</h3> -->
 		<div class="cicurloGrandre circuloFronted circuloLateral">
 			<div class="circuloCentral">
 				<h5>Lenguajes front-end</h5>
@@ -197,10 +201,20 @@
 				<span>XML</span>
 			</div>
 		</div>
-	</div>
-	<div class="extra">
-		
-	</div>
+	</section>
+	<section class="certificaciones">
+		<h3>Certificaciones</h3>
+		<div class="divContenedorCertificaciones">
+			<div class="certificacion">
+				<img src="img/logo_microsoft_azure_fundamentals.png">
+				<h4>Microsoft azure fundamentals (az-900)</h4>
+				<p>Diciembre de 2024</p>
+			</div>
+		</div>
+	</section>
+	<section class="extra">
+
+	</section>
 	<script type="text/javascript">
 		var i = 0;
 		var txt = "Javier Renedo";
