730eb4fedabe7cbe58a916a523069fd4b3376196
renedogj <renedogj@gmail.com>
2025-04-02 15:52:59 +0200

    add icon and last logos

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<title>Javier Renedo</title>
 	<meta name=author content="Javier Renedo">
 
-	<!-- <meta http-equiv="refresh" content="1"> -->
+	<link rel="icon" type="image/x-icon" href="img/renedo.png">
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
